337d1a084acba19daa4926ffc1b4fb249e800054
PhuriphatYoosuwan <phuriphat_yoosuwan@cmu.ac.th>
2025-02-16 14:01:10 +0700

    add react

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+      <div className="flex items-center justify-center min-h-screen bg-white">
+        <h1 className="text-4xl font-bold text-center text-black">
+          Hello World
+        </h1>
+      </div>
+  );
+}
