007b3ee0fc931977899d5af18bb61526492d63d5
navyney <141761309+navyney@users.noreply.github.com>
2025-02-20 02:39:10 +0700

    added initialHex_Ally={[11, 12, 13, 21, 22]}                          initialHex_Opponent={[77, 78, 86, 87, 88]} in hexMap after merged bc เดี๋ยวบัค

--- frontend/src/app/hexMap/page.tsx
+++ frontend/src/app/hexMap/page.tsx
@@ -1,9 +1,11 @@
-import HexGrid from "@/component/HexGrid"; // เปลี่ยน path ให้ถูกต้อง
+import HexGrid from "@/component/HexGrid";
 
 export default function Home() {
     return (
         <main>
-            <HexGrid rows={8} cols={8} size={40} distance={20} />
+            <HexGrid rows={8} cols={8} size={40} distance={20}
+                     initialHex_Ally={[11, 12, 13, 21, 22]}
+                     initialHex_Opponent={[77, 78, 86, 87, 88]}/>
         </main>
     );
 }
