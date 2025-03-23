f2511496fc12f6ce21e8fe6a6bc54b1a89266ba0
Sirawit Seetong <sirawit_se@cmu.ac.th>
2025-03-23 21:57:57 +0700

    fix ally & nearby expr (direction odd col & even col)

--- src/main/java/backend/KOMBOOD/expression/AllyExpr.java
+++ src/main/java/backend/KOMBOOD/expression/AllyExpr.java
@@ -11,28 +11,61 @@ public class AllyExpr implements Expr {
 
     @Override
     public long eval(Minion minion) throws EvalError {
-        int row = minion.getRow();
-        int col = minion.getCol();
+        int row = minion.getRow()-1;
+        int col = minion.getCol()-1;
         for(int dis = 1;dis < 8;dis++){
-            if(minion.onMap(row-dis,col) && minion.getOwner().getMinion().contains(minion.getMap().getMinionAt(row-dis,col))){
-                return dis*10+1;
+            if(col%2 == 1) {
+
+                if(minion.onMap(row-dis,col) && minion.getOwner().getMinion().contains(minion.getMap().getMinionAt(row - dis,col))){
+                    System.out.println(1);
+                    return dis*10 + 1;
+                }
+                else if (minion.onMap(row , col + dis) && minion.getOwner().getMinion().contains(minion.getMap().getMinionAt(row , col + dis))) {
+                    System.out.println(2);
+                    return dis * 10 + 2;
+                }
+                else if (minion.onMap(row+dis, col + dis) && minion.getOwner().getMinion().contains(minion.getMap().getMinionAt(row+dis, col + dis))) {
+                    System.out.println(3);
+                    return dis * 10 + 3;
+                }
+                else if(minion.onMap(row+dis,col) && minion.getOwner().getMinion().contains(minion.getMap().getMinionAt(row+dis,col))){
+                    System.out.println(4);
+                    return dis*10+4;
+                }
+                else if(minion.onMap(row+dis,col-dis) && minion.getOwner().getMinion().contains(minion.getMap().getMinionAt(row+dis,col-dis))){
+                    System.out.println(5);
+                    return dis*10+5;
+                }
+                else if(minion.onMap(row,col-dis) && minion.getOwner().getMinion().contains(minion.getMap().getMinionAt(row,col-dis))){
+                    System.out.println(6);
+                    return dis*10+6;
+                }
             }
-            else if(minion.onMap(row-dis,col+dis) && minion.getOwner().getMinion().contains(minion.getMap().getMinionAt(row-dis,col+dis))){
-                return dis*10+2;
-            }
-            else if(minion.onMap(row+dis,col+dis) && minion.getOwner().getMinion().contains(minion.getMap().getMinionAt(row+dis,col+dis))){
-                return dis*10+3;
-            }
-            else if(minion.onMap(row+dis,col) && minion.getOwner().getMinion().contains(minion.getMap().getMinionAt(row+dis,col))){
-                return dis*10+4;
-            }
-            else if(minion.onMap(row+dis,col-dis) && minion.getOwner().getMinion().contains(minion.getMap().getMinionAt(row+dis,col-dis))){
-                return dis*10+5;
-            }
-            else if(minion.onMap(row-dis,col-dis) && minion.getOwner().getMinion().contains(minion.getMap().getMinionAt(row-dis,col-dis))){
-                return dis*10+6;
-            }
-            else{
+            else if(col %2 == 0){
+                if(minion.onMap(row-dis,col) && minion.getOwner().getMinion().contains(minion.getMap().getMinionAt(row-dis,col))){
+                    System.out.println(1);
+                    return dis*10+1;
+                }
+                else if (minion.onMap(row - dis, col + dis) && minion.getOwner().getMinion().contains(minion.getMap().getMinionAt(row - dis, col + dis))) {
+                    System.out.println(2);
+                    return dis * 10 + 2;
+                }
+                else if (minion.onMap(row, col + dis) && minion.getOwner().getMinion().contains(minion.getMap().getMinionAt(row, col + dis))) {
+                    System.out.println(3);
+                    return dis * 10 + 3;
+                }
+                else if(minion.onMap(row+dis,col) && minion.getOwner().getMinion().contains(minion.getMap().getMinionAt(row+dis,col))){
+                    System.out.println(4);
+                    return dis*10+4;
+                }
+                else if(minion.onMap(row,col-dis) && minion.getOwner().getMinion().contains(minion.getMap().getMinionAt(row,col-dis))){
+                    System.out.println(5);
+                    return dis*10+5;
+                }
+                else if(minion.onMap(row-dis,col-dis) && minion.getOwner().getMinion().contains(minion.getMap().getMinionAt(row-dis,col-dis))){
+                    System.out.println(6);
+                    return dis*10+6;
+                }
             }
         }
         return 0;
